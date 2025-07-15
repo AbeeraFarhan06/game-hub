3f0bbb575e23db1958eb8b29b6b29a1e8536484d
Abeera Farhan <mecard2021@gmail.com>
2025-07-15 17:12:58 +0500

    Build a NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react"
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  
   //templateAreas is to define layout of our page
-  //each string represents a row 
+  //each string represents a row
   //and within string space shows start of new coloumn
-  return <Grid templateAreas={ {
-    base: `"nav" "main" ` ,
-    lg: `"nav nav" "aside main" ` //1024px
-  } }>
-    <GridItem area='nav' bg='coral'>Nav</GridItem>
-    <Show above="lg">
-      <GridItem area='aside' bg='gold'>Aside</GridItem>
-    </Show>
-    <GridItem area='main' bg='dodgerblue'>Main</GridItem>
-  </Grid>
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main" `,
+        lg: `"nav nav" "aside main" `, //1024px
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
