94e8884818e215a17d70c5f04d61ed81b5f9585f
Abeera Farhan <mecard2021@gmail.com>
2025-07-16 10:39:50 +0500

    Build Platform Selector

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
+import PlatformSelector from "./components/PlatformSelector";
 
 function App() {
 
@@ -32,6 +33,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main" >
+        <PlatformSelector />
         <GameGrid selectedGenre={selectGenre}/>
       </GridItem>
     </Grid>
