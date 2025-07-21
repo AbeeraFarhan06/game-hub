e52c48073ce39994a9aca90ad06a43537c0418d3
Abeera Farhan <mecard2021@gmail.com>
2025-07-21 19:12:29 +0500

    Fetch Game Details

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,7 +1,21 @@
+import { useParams } from "react-router-dom"
+import useGam from "../hooks/useGam"
+import { Heading, Spinner, Text } from "@chakra-ui/react"
 
 const GameDetailPage = () => {
+
+  const {slug} = useParams()
+  const {data: game, isLoading, error} = useGam(slug!)
+
+  if (isLoading) return <Spinner />
+
+  if (error || !game) throw error
+
   return (
-    <div>GameDetailPage</div>
+    <>
+      <Heading>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
+    </>
   )
 }
 
